67376241ae94eff2b6df3219755aa90bc2e9f9be
MattHelton <mjoelhelton@me.com>
2019-07-06 23:27:43 -0400

    Adds devise and devise User

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'nomster-matt-helton.herokuapp.com' }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
